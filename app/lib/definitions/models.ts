1d430f675f786be483446254e9c713db5c47e782
Marius <grosskopf81@gmail.com>
2024-11-16 14:46:07 +0100

    add models loader

---
+++ app/lib/definitions/models.ts
@@ -0,0 +1,14 @@
+import ItemModel from "./item.definitions";
+import SlotModel from "./slot.definitions";
+import UserModel from "./user.definitioins";
+import PlaceModel from "./place.definitions";
+import CategoryModel from "./category/category.definitions";
+
+// Exportiere alle Modelle für einfachen Zugriff
+export {
+  ItemModel,
+  SlotModel,
+  UserModel,
+  PlaceModel,
+  CategoryModel
+};
